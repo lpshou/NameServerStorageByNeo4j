7e7df99a4be74794ba28b7ad46080a437b7c6b1d
lpshou <lp_shou@163.com>
2013-12-09 10:55:34 +0800

    修改userTest

--- src/com/b409/nameServer/namespace/api/User.java
+++ src/com/b409/nameServer/namespace/api/User.java
@@ -51,32 +51,31 @@ public class User {
 			System.out.println("删除用户"+userName+"成功：该用户本来就不存在！");
 			return 0;
 		}else{
-
-				List<String> relationshipTypes = new ArrayList<String>();
-				relationshipTypes.add("contains");
-				relationshipTypes.add("create");
-				
-				List<Integer> relationshipIds = relationship.getRelationshipIdsOfOneNode(userId, "out", relationshipTypes);
+			List<String> relationshipTypes = new ArrayList<String>();
+			relationshipTypes.add("contains");
+			relationshipTypes.add("create");
+			
+			List<Integer> relationshipIds = relationship.getRelationshipIdsOfOneNode(userId, "out", relationshipTypes);
 //				System.out.println(relationshipIds.size());
-				//如果用户拥有文件或创建了组，则无法删除
-				if(relationshipIds.size()!=0){
-					System.out.println("删除用户"+userName+"失败：请先删除用户拥有的文件和用户创建的组");
-					return -1;
-				}else {
-					//删除其他组、文件、用户和该用户之间的关系
-					List<String>relationshipTypeStrings = new ArrayList<>();
-					relationshipTypeStrings.add("contains");
-					relationshipTypeStrings.add("like");
-					relationshipTypeStrings.add("friend");
-					List<Integer> relationshipIdslList = relationship.getRelationshipIdsOfOneNode(userId, "all", relationshipTypeStrings);
-					for(int i=0;i<relationshipIdslList.size();i++){
-						relationship.deleteRelationship(relationshipIdslList.get(i));
-					}
-					//删除这个节点
-					namespace.deleteNode(userId);
-					System.out.println("删除用户"+userName+"成功");
-					return 0;
+			//如果用户拥有文件或创建了组，则无法删除
+			if(relationshipIds.size()!=0){
+				System.out.println("删除用户"+userName+"失败：请先删除用户拥有的文件和用户创建的组");
+				return -1;
+			}else {
+				//删除其他组、文件、用户和该用户之间的关系
+				List<String>relationshipTypeStrings = new ArrayList<>();
+				relationshipTypeStrings.add("contains");
+				relationshipTypeStrings.add("like");
+				relationshipTypeStrings.add("friend");
+				List<Integer> relationshipIdslList = relationship.getRelationshipIdsOfOneNode(userId, "all", relationshipTypeStrings);
+				for(int i=0;i<relationshipIdslList.size();i++){
+					relationship.deleteRelationship(relationshipIdslList.get(i));
 				}
+				//删除这个节点
+				namespace.deleteNode(userId);
+				System.out.println("删除用户"+userName+"成功");
+				return 0;
+			}
 		}
 	}
 }
--- src/com/b409/nameServer/test/UserTest.java
+++ src/com/b409/nameServer/test/UserTest.java
@@ -4,11 +4,14 @@ import com.b409.nameServer.namespace.api.User;
 
 public class UserTest {
 	public static void main(String args[]){
+		long start=System.currentTimeMillis();
 		User user = new User();
 		user.createUser("zhangsan");
 		user.createUser("lisi");
 		user.createUser("wangwu");
 //		user.deleteUser("zhangsan");
 //		user.deleteUser("lisi");
+		long time = System.currentTimeMillis() - start;
+		System.out.println("运行耗时= "+time+" 毫秒");
 	}
 }
